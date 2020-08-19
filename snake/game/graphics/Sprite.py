78908345e588cac4b88cac771960ccf677ae6976
Jonathyn Stiverson <jstiverson2002@gmail.com>
2020-08-19 17:35:12 -0500

    Updated files.
    Removed testing from Sprite.py.
    - Wouldn't be able to test in the same way for classes that depend on
    the Sprite class. If you're not doing it the same way for everything,
    don't do it for just one.
    
    Added testing to Main.py.
    - This is to test and make sure the application works during
    development. Eventually the actual 'testing' will be moved to
    test(s).py.

--- snake/game/graphics/Sprite.py
+++ snake/game/graphics/Sprite.py
@@ -158,36 +158,3 @@ class Sprite:
         :param surface: The pygame.Surface object that the polygons will be drawn onto.
         """
         pass
-
-
-def test_sprite():
-    """
-    Tests the Sprite class to confirm it works as intended.
-    """
-    pygame.init()
-    screen = pygame.display.set_mode((500,500))
-    surface = pygame.Surface(screen.get_size())
-    pygame.display.set_caption('Testing')
-    clock = pygame.time.Clock()
-    running = True
-    
-    test_rects = [[pygame.Rect(20, 20, 30, 30), pygame.Color(255,255,255)], [pygame.Rect(120, 20, 30, 30), pygame.Color(255, 255, 255)]]
-    sprite = Sprite('polygon', (0, 0), polygons = test_rects)
-
-    while running:
-        clock.tick(60)
-        sprite.position = (sprite.x + 3, sprite.y + 3)
-        sprite.update()
-        sprite.update_pos_vars()
-        surface.fill((0, 0, 0))
-        sprite.draw(surface)
-        screen.blit(surface, (0, 0))
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-
-
-if __name__ == '__main__':
-    test_sprite()
