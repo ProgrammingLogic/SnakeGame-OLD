9b70eabe99a841230c8415f29bb72652c4790fd0
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2023-10-01 14:59:13 -0500

    Setup logger

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,13 +1,42 @@
+import logging
+
+
+def setup_logger():
+    base_log_level = logging.DEBUG
+    logger = logging.getLogger(__name__)
+    logger.setLevel(base_log_level)
+    log_file = "./snake.log"
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    file_log_level = base_log_level
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(file_log_level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    console_log_level = base_log_level
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(console_log_level)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    logger.debug("Logger has been initialized")
+
+
 def main():
     # Logic to start the game
+    setup_logger()
+    logger = logging.getLogger(__name__)
+
     running = True
     iterations = 0
     max_iterations = 10
-    print("Starting game...")
+    logger.info("Starting game")
 
 
     while running:
-        print(f"""Iteration {iterations}""")
+        logger.debug(f"""Iteration {iterations}""")
         iterations += 1
         
         # Determine if we should stop the game
@@ -16,7 +45,7 @@ def main():
 
 
     # Logic to end the game 
-    print("Ending game...")
+    logger.info("Ending game")
 
 
 if __name__ == '__main__':
