cb5ffc05ee37bc51af30e1f37b7807ed59776ad0
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2023-10-01 14:44:22 -0500

    Create logic for game loop

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,5 +1,22 @@
 def main():
-    print("Hello world!")
+    # Logic to start the game
+    running = True
+    iterations = 0
+    max_iterations = 10
+    print("Starting game...")
+
+
+    while running:
+        print(f"""Iteration {iterations}""")
+        iterations += 1
+        
+        # Determine if we should stop the game
+        if (iterations > max_iterations):
+            running = False
+
+
+    # Logic to end the game 
+    print("Ending game...")
 
 
 if __name__ == '__main__':
